2660fc85feed618bb37d206cdea243a9a28da762
MayDanYing <tasya757@gmail.com>
2020-11-17 18:10:57 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/lesson2/SimpleClass.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.lesson2;
+
+public class SimpleClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is project with Maven Quickstart");
+    }
+}
